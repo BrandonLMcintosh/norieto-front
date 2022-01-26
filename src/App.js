71d6ec1abf7e34e12304fadfaa2cd3f52e5aa589
Brandon McIntosh <brandon.l.mcintosh@gmail.com>
2022-01-25 20:41:06 -0800

    Structuring App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./Routes";
 import NorietoApi from "./api";
 import useToken from "./hooks/useToken";
 import "./App.css";
 import jwt from "jsonwebtoken";
-import UserContext from "./UserContext";
+import UserContext from "./components/auth/UserContext/UserContext";
 import Header from "./components/nav/header/Header";
 import Footer from "./components/nav/footer/Footer";
 
@@ -19,18 +19,68 @@ function App() {
   const [map, setMap] = useState({});
 
   useEffect(() => {
-    async function getUser(){
-      if(token){
-        try{
-          let {username} = jwt.decode(token);
+    async function getUser() {
+      if (token) {
+        try {
+          let { username } = jwt.decode(token);
           let user = await NorietoApi.userGet(username);
           setUser(user);
           setMap(user.map);
-          
+        } catch (err) {
+          console.error("Issue loading user");
+          setUser(null);
         }
       }
+      setLoaded(true);
     }
-  })
+    setLoaded(false);
+    getUser();
+  }, [token]);
+
+  async function register(data) {
+    try {
+      const token = await NorietoApi.authRegisterd(data);
+      setToken(token);
+      return { success: true };
+    } catch (err) {
+      console.error("failed: singup", err);
+      return { success: false, err };
+    }
+  }
+
+  async function login(data) {
+    try {
+      let token = await NorietoApi.authLogin(data);
+      setToken(token);
+      return { success: true };
+    } catch (err) {
+      console.error("failed login", err);
+      return { success: false, err };
+    }
+  }
+
+  function logout() {
+    setUser(null);
+    setToken(null);
+  }
+
+  function deleteMap() {
+    setMap({});
+  }
+
+  if (!loaded) return <Loading />;
+
+  return (
+    <BrowserRouter>
+      <UserContext.Provider value={{user, setUser}}>
+        <div className="App">
+          <Header />
+          <Routes login={login} register={register}/>
+          <Footer />
+        </div>
+      </UserContext>
+    </BrowserRouter>
+  );
 }
 
 export default App;
